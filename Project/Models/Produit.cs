bdd6a25a61c46f3569ec8887811553df7546b5d2
Dell <Dell@DESKTOP-8MV9MLF>
2023-12-23 00:29:32 +0000

    Add project files.

---
+++ Project/Models/Produit.cs
@@ -0,0 +1,15 @@
+﻿namespace Project.Models
+{
+    public class Produit
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public float prix { get; set; }
+        public string Brand { get; set; }
+        public string Disponibilite { get; set;}
+        public string Puissance { get; set; }
+        public string Review { get; set;  }
+
+
+    }
+}
